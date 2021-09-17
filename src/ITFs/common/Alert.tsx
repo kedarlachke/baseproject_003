14653e61cb634181742ea5e2baa1e31e0a3dbe70
kadlac <53255756+kedarlachke@users.noreply.github.com>
2021-09-18 01:58:39 +0530

    modification of alert component

--- src/ITFs/common/Alert.tsx
+++ src/ITFs/common/Alert.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './alert.css'
-function Alert(props:any) {
+function Alert({type}:any) {
     const [displayAlert, setDisplayAlert] = useState(false);
     useEffect(() => {
         setDisplayAlert(true)
         return () => {
             
         }
-    }, [props.type])
+    }, [type])
     return (
-        <div className={displayAlert?"alert warning show":"alert success hide"}>
-            <span className="fas fa-exclamation-circle"></span>
+        <div className={displayAlert?`alert ${type} show`:`alert ${type} hide`}>
+            <span className={type === 'success'? "fas fa-check-circle":"fas fa-exclamation-circle"}></span>
             <span className="msg">Warning: This is a warning alert</span>
             <span className="close-btn" onClick={()=>setDisplayAlert(false)}>
                 <span className="fas fa-times"></span>
